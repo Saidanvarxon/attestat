ab70e02e0abaf643d7b2b2ff591770a939f655ca
Saidanvarxon <Saidanvarxan@gmail.com>
2024-06-03 14:15:44 +0500

    finish

--- obj/Debug/net8.0/attestat.AssemblyInfo.cs
+++ obj/Debug/net8.0/attestat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("attestat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae51ff582ec731abc743258ea59f5fd7e13b721a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73da362a1195cdeab29395c0f65ebbbba37a7b66")]
 [assembly: System.Reflection.AssemblyProductAttribute("attestat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("attestat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
